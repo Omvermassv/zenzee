fb2072001a8cbc507d83e11a952d85bdca0d59ea
Om Verma <omvermassv@gmail.com>
2024-03-13 23:51:51 +0530

    Added regex checking in registration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />}></Route>
         <Route path="/Register" element={<Register />}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
       </Routes>
     </Router>
   );
